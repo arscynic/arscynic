2f100529852b2cc99fffa8d5eab7f669e55aec41
Angelino Desmet <desmet@vivaldi.net>
2024-11-02 23:31:49 +0100

    permanent and instant dark mode

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,13 +1,9 @@
-function toggle_dark_mode() {
-  var element = document.body;
-  element.classList.toggle("dark_mode");
-
-  // Save the current mode in localStorage
-  if (element.classList.contains("dark_mode")) {
-    localStorage.setItem("dark_mode", "enabled");
-  } else {
-    localStorage.setItem("dark_mode", "disabled");
-  }
+const DARK_BG_COLOR = '#191919';
+const COLOR_AWHITE = 'antiquewhite'
+// Immediately set the CSS variables based on the dark mode preference
+if (localStorage.getItem("dark_mode") === "enabled") {
+  document.documentElement.style.setProperty('--bg-color', DARK_BG_COLOR);
+  document.documentElement.style.setProperty('--fg-color', COLOR_AWHITE);
 }
 
 // Check localStorage on page load and apply the preferred mode
@@ -16,6 +12,32 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.add("dark_mode");
   }
 });
+
+function toggle_dark_mode() {
+  var element = document.body;
+  element.classList.toggle("dark_mode");
+  const root = document.documentElement;
+  const isDarkMode = localStorage.getItem("dark_mode") === "enabled";
+
+    // Save the current mode in localStorage
+    if (element.classList.contains("dark_mode")) {
+      localStorage.setItem("dark_mode", "enabled");
+    } else {
+      localStorage.setItem("dark_mode", "disabled");
+    }
+
+  if (isDarkMode) {
+    // Switch to light mode
+    root.style.setProperty('--bg-color', COLOR_AWHITE);
+    root.style.setProperty('--fg-color', 'black');
+    localStorage.setItem("dark_mode", "disabled");
+  } else {
+    // Switch to dark mode
+    root.style.setProperty('--bg-color', DARK_BG_COLOR);
+    root.style.setProperty('--fg-color', COLOR_AWHITE);
+    localStorage.setItem("dark_mode", "enabled");
+  }
+}
 
 /* Sources: 
 Dark mode: https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
