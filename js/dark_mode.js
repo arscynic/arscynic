8e3e0d974080708e779ed32e0b51d710e0af1464
Angelino Desmet <desmet@vivaldi.net>
2024-11-02 13:57:39 +0100

    function name change, adds dark mode button tooltip

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,18 +1,22 @@
-function toggle_light_dark() {
+function toggle_dark_mode() {
   var element = document.body;
   element.classList.toggle("dark_mode");
 
   // Save the current mode in localStorage
   if (element.classList.contains("dark_mode")) {
-    localStorage.setItem("darkMode", "enabled");
+    localStorage.setItem("dark_mode", "enabled");
   } else {
-    localStorage.setItem("darkMode", "disabled");
+    localStorage.setItem("dark_mode", "disabled");
   }
 }
 
 // Check localStorage on page load and apply the preferred mode
 document.addEventListener("DOMContentLoaded", function() {
-  if (localStorage.getItem("darkMode") === "enabled") {
+  if (localStorage.getItem("dark_mode") === "enabled") {
     document.body.classList.add("dark_mode");
   }
 });
+
+/* Sources: 
+Dark mode: https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
+Persistency: ChatGPT */
